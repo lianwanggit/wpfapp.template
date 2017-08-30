5dbda26b66c1ab78fe7eabbc31e36724c71e4264
Lian Wang <kohlemo@gmail.com>
2017-08-30 21:42:02 +1200

    relocation

---
+++ src/WpfApp.Template.Domain/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WpfApp.Template.Domain
+{
+	public class Person
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public bool Gender { get; set; }
+		public DateTimeOffset DateOfBirth { get; set; }
+	}
+}
